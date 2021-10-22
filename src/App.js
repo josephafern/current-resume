7f1f079e82e4a9e5daeb696cbb620d5522b0e864
josephafern <60451011+josephafern@users.noreply.github.com>
2021-10-22 13:35:54 -0400

    First commit, mostly done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import React from 'react';
+import seal from './seal-light-black.png';
 import './App.css';
+import { FaGamepad } from 'react-icons/fa';
+import { MdFastfood } from 'react-icons/md';
+import { ImBriefcase } from 'react-icons/im';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export class Resume extends React.Component {
+	constructor(props){
+		super(props);
+	}
+	
+	render(){
+		return (
+    <div>
+      
+		<div className='overall'>
+		<div className="header">
+				<h1>Joseph Aurelio Fernandez</h1>
+		</div>
+		<div className="nav">
+			<a href="#objective"><div className="nav-btn single-word">Objective</div></a>
+			<a href="#education"><div className="nav-btn single-word">Education</div></a>
+			<a href="#rel-exp"><div className="nav-btn single-word">Experience</div></a>
+			
+			<a href="#portfolio"><div className="nav-btn single-word">Portfolio</div></a>
+		</div>
+		<div className="main">
+				<div id='objective'></div>
+				<div className="objective">
+					<h2>
+						<span>Objective:</span>
+					</h2>
+			<div id="objective-grid">
+				<img src={seal}/>
+				<p>Stanford graduate skilled in analytical research, project management and content editing, looking to learn from and contribute to the success of an organization.</p>	
+			</div>
+			</div>
+			<div id='education'></div>
+			<div className="education">
+				<h2><span>Education:</span></h2>
+				<div className="year-row">
+					<div className="left">
+						Sept. 2012 - Jun. 2016:
+					</div>
+					<div className="right">
+						<span>Stanford University:</span>
+						<br/>
+						Combined Major in Philosophy & Religious Studies
+					</div>
+					<div className="left">
+						Aug. 2008 - Jun. 2012:
+					</div>
+					<div className="right">
+						<span>Maritime & Science Technology (MAST) Academy</span>
+						<br/>
+						Concentration in Oceanic and Atmospheric Sciences
+					</div>
+				</div>
+			</div>
+			<div id='rel-exp'></div>
+			<div className="rel-exp">
+				<h2><span>Experience:</span></h2>
+				<h3>Computer Science Background - </h3>
+				<p>Core CS curriculum at Stanford. These courses 
+				provided an extensive experience in systems architecture,
+				memory management and performance optimization. I am 
+				proficient in HTML, CSS and (soon to be) Javascript.</p>
+				<h3>Pre-Colonial & Contemporary Philosophy - </h3>
+				<p>Series of courses focused on philosophical ideologies
+				and the sociological implications of their effects on human 
+				history and behavior. Maintaining a concentration in pre-modern 
+				Taoist, Existential and Romantic philosophies, I've gained a 
+				unique perspective on different theories of ontology and 
+				sociology, acquiring insight into historical movements and their 
+				relation to both ancient and contemporary modes of thought. 
+				Through this program, I've been given thorough experience in 
+				critical analysis, writing and editing.</p>
+			</div>
+			<div id='portfolio'></div>
+			<div className='portfolio'>
+				<h2><span>Portfolio:</span></h2>
+				<div className='port-flex'>
+          <a className='project-link' href='https://josephafern.github.io/bombermanjs'><FaGamepad className='project-icon' /></a>
+          <a className='project-link' href='https://empty-chairs.herokuapp.com'><MdFastfood className='project-icon' /></a>
+          <a className='project-link' href='https://blindr-job.herokuapp.com'><ImBriefcase className='project-icon' /></a>
+				</div>
+			</div>
+		</div>
+		</div>
     </div>
   );
+	
+	}
+
 }
 
-export default App;
+export default Resume;
